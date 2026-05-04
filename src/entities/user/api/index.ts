29d210a7c7be02365eb7c584a914a35a6cd50139
Dmitry Seregin <dimenerDS@gmail.com>
2026-05-04 12:10:00 +0400

    Change api

--- src/entities/user/api/index.ts
+++ src/entities/user/api/index.ts
@@ -1,10 +1,61 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { API_URL } from "../../../constants";
-import type {
-  ApiResponse,
-  UserFormData,
-  UserType,
-} from "../../../shared/types";
+import type { UserFormData, UserType } from "../../../shared/types";
 
+export const userApi = createApi({
+  reducerPath: "userApi", // Имя ключа в сторе
+  baseQuery: fetchBaseQuery({ baseUrl: API_URL }), // Базовый URL для всех запросов
+
+  // "Метки" для автоматического обновления данных (кэша)
+  tagTypes: ["User"],
+
+  endpoints: (builder) => ({
+    // 1. Получение всех пользователей
+    getUsers: builder.query<UserType[], void>({
+      query: () => "/users",
+      providesTags: ["User"], // Говорим: "этот запрос зависит от метки User"
+    }),
+
+    // 2. Создание пользователя
+    /* builder.mutation<ResultType, RequestArg> 
+        ResultType что придет в ответе от сервера (Response Body)  
+        RequestArg это тип аргумента, который ты передаешь в хук (Request Body / Query Params)*/
+    addUser: builder.mutation<UserType, UserType>({
+      query: (newUser) => ({
+        url: "/users",
+        method: "POST",
+        body: newUser,
+      }),
+      invalidatesTags: ["User"], // Очищает кэш User, заставляя getUsers перезапроситься
+    }),
+
+    // Delete
+    deleteUser: builder.mutation<void, string>({
+      query: (id) => ({
+        url: `/users/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["User"],
+    }),
+
+    // Update
+    updateUser: builder.mutation<
+      UserType,
+      { id: string; userData: UserFormData }
+    >({
+      query: ({ id, userData }) => ({
+        url: `/users/${id}`,
+        method: "PUT",
+        body: userData, // Передаем объект напрямую, без JSON.stringify
+      }),
+      invalidatesTags: ["User"],
+    }),
+  }),
+});
+
+export const { useGetUsersQuery, useDeleteUserMutation } = userApi;
+
+/*
 const getErrorMessage = (status: number) => {
   if (status >= 500) return "Server error. Please try again later.";
   if (status === 404) return "Resource not found.";
@@ -90,3 +141,6 @@ export const updateUser = async (
     signal,
   });
 };
+
+
+*/
